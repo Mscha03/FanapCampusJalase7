c4be1f584abd96c8a158644e2379f8f722cd5371
mscha <m.saleh.sedeghi@gmail.com>
2024-02-07 01:49:25 +0330

    Initial commit

---
+++ src/main/java/org/example/Log/Main.java
@@ -0,0 +1,21 @@
+package org.example.Log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+
+        log.error("error");
+        log.info("info");
+        log.debug("debug");
+        log.trace("trace");
+        log.debug("Hello this is a debug message");
+        log.info("Hell this is an info message");
+
+    }
+}
